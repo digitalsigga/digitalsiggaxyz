94b41508191353ac7fb717b768ee192086aed2c0
digitalsigga <sigga.irl@digitalsigga.com>
2024-05-24 09:06:48 +0000

    feat : added fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Sigríður Birna Matthíasdóttir
-        </p>
-
-      </div>
-
+      <h1>Hello</h1>
     </main>
   );
 }
