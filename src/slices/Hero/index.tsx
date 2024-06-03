b2f3cee2d3cf8e9876a27a0c4d43696087ab9b1d
digitalsigga <sigga.irl@digitalsigga.com>
2024-06-03 14:23:02 +0000

    fix: css missing

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,6 +1,7 @@
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import styles from './Hero.module.css';
 
 /**
  * Props for `Hero`.
@@ -16,7 +17,11 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <PrismicRichText field={slice.primary.heading} />
+      <PrismicRichText field={slice.primary.heading} components={{
+        heading1: ({children})=>(
+          <h1 className={styles.heading}>{children}</h1>
+        )
+      }}/>
 
       <PrismicRichText field={slice.primary.body} />
 
