b4d6aaa9e3922e0dbb2b1ef948cb02a607c1dada
digitalsigga <sigga.irl@digitalsigga.com>
2024-06-04 13:56:57 +0000

    fix: refactoring

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,11 +1,24 @@
 import { Content } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
-import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import { JSXMapSerializer, PrismicRichText, SliceComponentProps } from "@prismicio/react";
 import styles from './Hero.module.css';
 import Bounded from "@/components/Bounded";
+
+
+const components: JSXMapSerializer = {
+  heading1: ({children})=>(
+    <h1 className={styles.heading}>{children}</h1>
+  ),
+  paragraph: ({children})=>(
+    <p className={styles.paragraph}>{children}</p>
+  ),
+}
+
+
 /**
  * Props for `Hero`.
  */
+
 export type HeroProps = SliceComponentProps<Content.HeroSlice>;
 
 /**
@@ -18,19 +31,17 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       data-slice-variation={slice.variation}
     >
 
-      <PrismicRichText field={slice.primary.heading} components={{
-        heading1: ({children})=>(
-          <h1 className={styles.heading}>{children}</h1>
-        )
-      }}/>
+      <PrismicRichText 
+      field={slice.primary.heading} 
+      components={components}
+      />
 
-      <PrismicRichText field={slice.primary.body} components={{
-        paragraph: ({children})=>(
-          <p className={styles.paragraph}>{children}</p>
-        )
-      }}/>
+      <PrismicRichText 
+      field={slice.primary.body} 
+      components={components}
+      />
 
-        <PrismicNextImage field={slice.primary.image} className={styles.image}/>
+      <PrismicNextImage field={slice.primary.image} className={styles.image}/>
 
     </Bounded>
   );
