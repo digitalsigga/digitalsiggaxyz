be6d5cd38e0caaf29f2023c12ceda0bbdb4ce8fa
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-02 14:34:02 +0000

    breyta

---
+++ digitalsigga/src/pages/App.tsx
@@ -0,0 +1,16 @@
+import '../styles/App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+      </div>
+      <h1>Digital Sigga is a digital fashion designer and a front end web developer</h1>
+      <div className="card">
+      </div>
+    </>
+  )
+}
+
+export default App
