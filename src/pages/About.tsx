998df34a0afaf4314fec8ac56238971a8f22078e
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-04 09:42:05 +0000

    more css

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,7 +3,7 @@ import "../styles/about.css";
 function About() {
   return (
     <div className="aboutContainer">
-      <h1>About Me</h1>
+      <h1 className="rafael">About Me</h1>
       <p>This is the about page content.</p>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,14 @@ import "../styles/home.css";
 function Home() {
   return (
     <div className="homeContainer">
-      <h1 className="rafael">HHoho</h1>
-      <p>lala</p>
-      <h1>HHoho</h1>
-      <h1>HHoho</h1>
-      <h1>HHoho</h1>
-      <h1>HHoho</h1>
+      <h1 className="rafael">
+        Digital Sigga is balbalbalblalbalsffghjkuyg kasdjkasldjasldjasl
+        lkjdlkasjdlsaj lajsd la
+      </h1>
+      {/* <h1>is a digital fashion</h1>
+      <h1 className="rafael">designer</h1>
+      <h1>and a web</h1>
+      <h1 className="rafael">developer</h1> */}
     </div>
   );
 }
