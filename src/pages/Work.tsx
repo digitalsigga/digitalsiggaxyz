9c34949ff073e85d7b2e190263b2ac7fa6f8ba93
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-05 14:03:15 +0000

    ein mynd

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -6,18 +6,29 @@ function Work() {
     <div>
       <div className="workContainer">
         <div className="card">
-          <Link to="/work/madesign">
+          <Link to="/work/digitalfashion">
             <div className="card1">
-              <h2>MA Design</h2>
-              <p>MA project work</p>
-              <p>2017-2019</p>
-              <img></img>
+              <div className="card1text">
+                <h2>Digital Fashion</h2>
+                <p>CLO 3D</p>
+                <p>2020-2024</p>
+              </div>
+              <div className="card1img">
+                <img
+                  src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                  alt="Woman wearing digital design with a moon sculpture on top of her head"
+                ></img>
+              </div>
             </div>
           </Link>
         </div>
         <div className="card">
-          <Link to="/work/digitalfashion">
-            <h2>Digital Fashion</h2>
+          <Link to="/work/madesign">
+            <div className="card2">
+              <h2>MA Design</h2>
+              <p>MA project work</p>
+              <p>2017-2019</p>
+            </div>
           </Link>
         </div>
         <div className="card">
