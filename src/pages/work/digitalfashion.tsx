0dcf45e6d754a475fd1d560fb3c11dc26bcefa3c
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-05 16:55:38 +0000

    better responsiveness

--- src/pages/work/digitalfashion.tsx
+++ src/pages/work/digitalfashion.tsx
@@ -1,18 +1,15 @@
-import React from "react";
 import "../../styles/digitalfashion.css"; // Import the CSS file
 
 function DigitalFashion() {
   const imageUrls = [
     "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png",
     "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon2_ny3vmi.png",
-    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon3_uqsbwr.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725554898/WaningMoon3_od0cdy.png",
     // Add more image URLs here if needed
   ];
 
   return (
     <div className="gallery">
-      <h2>Digital Fashion</h2>
-      <p>fassjjóóóón</p>
       <div className="gallery-grid">
         {imageUrls.map((url, index) => (
           <img key={index} src={url} alt={`Fashion ${index}`} />
