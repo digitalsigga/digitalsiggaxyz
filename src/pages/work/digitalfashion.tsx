95ab083f360658571d5a27a16033c7447feb729b
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-05 15:45:31 +0000

    pics

--- src/pages/work/digitalfashion.tsx
+++ src/pages/work/digitalfashion.tsx
@@ -1,8 +1,20 @@
+import "../../styles/digitalfashion.css";
+
 function DigitalFashion() {
+  // Number of images to display in the gallery
+  const numberOfImages = 12;
+  const imageUrl =
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png";
+
   return (
-    <div>
-      <h2>FAfa</h2>
+    <div className="gallery">
+      <h2>Digital Fashion</h2>
       <p>fassjjóóóón</p>
+      <div className="gallery-grid">
+        {Array.from({ length: numberOfImages }).map((_, index) => (
+          <img key={index} src={imageUrl} alt={`Fashion ${index}`} />
+        ))}
+      </div>
     </div>
   );
 }
