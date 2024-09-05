f5c2290b80e82bc04c64f28a3b46297d107d5f2e
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-05 15:37:42 +0000

    cards finally ok

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -7,50 +7,69 @@ function Work() {
       <div className="workContainer">
         <div className="card">
           <Link to="/work/digitalfashion">
-            <div className="card1">
-              <div className="card1text">
-                <h2>Digital Fashion</h2>
-                <p>CLO 3D</p>
-                <p>2020-2024</p>
-              </div>
-              <div className="card1img">
-                <img
-                  src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
-                  alt="Woman wearing digital design with a moon sculpture on top of her head"
-                ></img>
-              </div>
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
             </div>
           </Link>
         </div>
         <div className="card">
-          <Link to="/work/madesign">
-            <div className="card2">
-              <h2>MA Design</h2>
-              <p>MA project work</p>
-              <p>2017-2019</p>
+          <Link to="/work/digitalfashion">
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
             </div>
           </Link>
         </div>
         <div className="card">
-          <h2>✯</h2>
+          <Link to="/work/digitalfashion">
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
+            </div>
+          </Link>
         </div>
         <div className="card">
-          <h2>♤</h2>
+          <Link to="/work/digitalfashion">
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
+            </div>
+          </Link>
         </div>
         <div className="card">
-          <h2>♡</h2>
+          <Link to="/work/digitalfashion">
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
+            </div>
+          </Link>
         </div>
         <div className="card">
-          <h2>☂</h2>
-        </div>
-        <div className="card">
-          <h2>♜</h2>
-        </div>
-        <div className="card">
-          <h2>♡</h2>
-        </div>
-        <div className="card">
-          <h2>☂</h2>
+          <Link to="/work/digitalfashion">
+            <div>
+              <img
+                src="https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png"
+                alt="Woman wearing digital design with a moon sculpture on top of her head"
+              ></img>
+              <h2>Digital Fashion</h2>
+            </div>
+          </Link>
         </div>
       </div>
     </div>
