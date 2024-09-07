1a7fa51c91fde179f37129ac6762356196c91f56
digitalsigga <sigga.irl@digitalsigga.com>
2024-09-07 10:04:59 +0000

    fix : fleiri myndir

--- src/pages/work/digitalfashion.tsx
+++ src/pages/work/digitalfashion.tsx
@@ -2,9 +2,19 @@ import "../../styles/digitalfashion.css"; // Import the CSS file
 
 function DigitalFashion() {
   const imageUrls = [
-    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon1_lrljga.png",
-    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/WaningMoon2_ny3vmi.png",
-    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725554898/WaningMoon3_od0cdy.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/Digital%20Fashion/WaningMoon1_lrljga.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725544743/Digital%20Fashion/WaningMoon2_ny3vmi.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725554898/Digital%20Fashion/WaningMoon3_od0cdy.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703163/Digital%20Fashion/DSDB1_iumzyg.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703164/Digital%20Fashion/DSDB2_ez7xdj.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703165/Digital%20Fashion/DSDB3_sysd92.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703165/Digital%20Fashion/DSDB4_xsbta5.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703166/Digital%20Fashion/DSDB5_rjjbso.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703167/Digital%20Fashion/DSDB6_pc1t4j.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703167/Digital%20Fashion/DSDB7_lcuzpd.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703168/Digital%20Fashion/DSDB8_llbmgy.png",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725703169/Digital%20Fashion/DSDB9_j5wjgp.png",
+
     // Add more image URLs here if needed
   ];
 
--- src/pages/work/madesign.tsx
+++ src/pages/work/madesign.tsx
@@ -5,6 +5,9 @@ function MaDesign() {
     "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725555879/eg_nl05eq.jpg",
     "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725555871/Sigridur_Birna_Filtorial_Rakata_a3k1y1.jpg",
     "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725555870/Rakata_Acid_from_Sigri%CC%81%C3%B0ur_Birna_lf9fdv.jpg",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725700585/Editorial_6_hcavbw.jpg",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725700585/Editorial_3_oppjsl.jpg",
+    "https://res.cloudinary.com/dvlggqgbf/image/upload/v1725700585/Editorial_2_o0yf6b.jpg",
     // Add more image URLs here if needed
   ];
 
